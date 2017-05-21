a2ae294dff6217ef62fe4c15c176e4b55850dcef
Vink1 <15521076339@163.com>
2017-05-21 11:18:59 +0800

    first commit

---
+++ Model_example.php
@@ -0,0 +1,6 @@
+<?php
+class Model{
+	public static function getData(){
+		return $list;   //$list是一个二维关联数组
+	}
+}
---
+++ Modle.php
@@ -0,0 +1,14 @@
+<?php
+class Model
+{
+	public static function getData()
+	{
+		$data = [
+			['id'=>101,'name'=>'peter1','age'=>28,'salary'=>3001],
+			['id'=>102,'name'=>'peter2','age'=>28,'salary'=>3002],
+			['id'=>103,'name'=>'peter3','age'=>28,'salary'=>3003],
+			['id'=>104,'name'=>'peter4','age'=>28,'salary'=>3004],
+		];
+		return $data;
+	}
+}
